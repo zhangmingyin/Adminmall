7148d22ebeec37c9d52a8b79bb3f2eb73738fada
zhangmingyin <547805758@qq.com>
2020-08-19 22:51:46 +0800

    home页面基本布局

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,38 @@
 <template>
- <div>
-   <el-button type="text" @click="loginOut">退出登录</el-button>
- </div>
+  <el-container class="home_container">
+    <!-- 头部 -->
+    <el-header>
+      <div>后台管理系统</div>
+      <el-button type="info" @click="loginOut">退出登录</el-button>
+    </el-header>
+    <el-container>
+      <!-- 左侧边栏 -->
+      <el-aside width="200px">
+        <el-menu active-text-color="#ffd04b" background-color="#333744" text-color="#fff">
+          <!-- 一级菜单 -->
+          <el-submenu index="1">
+            <!-- 一级菜单的模板区域 -->
+            <template slot="title">
+              <!-- 图标 -->
+              <i class="el-icon-location"></i>
+              <!-- 导航文本 -->
+              <span>导航一</span>
+            </template>
+            <!-- 二级菜单 -->
+            <el-menu-item index="1-4-1">
+               <template slot="title">
+              <!-- 图标 -->
+                <i class="el-icon-location"></i>
+                <!-- 导航文本 -->
+                <span>导航一</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -40,6 +71,26 @@
 </script>
 
 <style scoped>
-
- 
+  .home_container{
+    height: 100%;
+  }
+  .el-header{
+    background-color: #373d41;
+    display: flex;
+    color: #fff;
+    font-size: 20px;
+    font-weight: bold;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .el-header div{
+    display: flex;
+    align-items: center;
+  }
+  .el-aside{
+    background-color: #333744;
+  }
+  .el-main{
+    background-color: #eaedf1;
+  }
 </style>
